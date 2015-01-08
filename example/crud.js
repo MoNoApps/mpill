3b70ab018483c1a0e8febea53e2b8ea24de666d0
Daniel Martinez <dmartz86@gmail.com>
2015-01-08 17:54:09 -0500

    Change throw error with console message.

--- example/crud.js
+++ example/crud.js
@@ -7,35 +7,35 @@ var myCompany = {name: 'MoNoApps'};
 
 // Ensure collection exists before drop it
 companies.CreateCollection(function(err, collection){
-  if (err) throw err;
+  if (err){ return console.log(err); }
   tpill.create(companies.NAME, collection.collectionName, 'CreateCollection', true);
 
   // Clean collection
   companies.DropCollection(function(err,results){
-    if (err) throw err;
+    if (err){ return console.log(err); }
     tpill.create(true, results, 'DropCollection', true);
 
     companies.Insert(myCompany,function(err,results){
-      if (err) throw err;
+      if (err){ return console.log(err); }
       myCompany =  results[0];
       tpill.create(myCompany.name, results[0].name, 'Insert', true);
 
       companies.Update({'_id': myCompany._id}, { $set : {name: 'MoNoApps LLC', upgrade: true} }, function(err,results){
-        if (err) throw err;
+        if (err){ return console.log(err); }
         tpill.create(1, results, 'Update', false);
 
         companies.FindOne({'_id': myCompany._id}, function(err,results){
-          if (err) throw err;
+          if (err){ return console.log(err); }
           tpill.create('MoNoApps LLC', results.name, 'FindOne', true);
 
           //var hex_value = '3ced938e'; this throws an error because is notinvalid
           var hex_value = myCompany._id.toString();
           companies.FindByObjectId({'_id': hex_value}, '_id', function(err,results){
-            if (err) throw err;
+            if (err){ return console.log(err); }
             tpill.create('MoNoApps LLC', results.name, 'FindByObjectId', true);
 
             companies.Count({}, function(err,results){
-              if (err) throw err;
+              if (err){ return console.log(err); }
               tpill.create(1, results, 'Count', true);
               tpill.create('number', typeof results, 'Finish Task', true);
 
